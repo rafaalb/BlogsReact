3a03566d28d91ebcb571c2fd72ccfe5615d618b1
Rafael Pina <rpina@solsteace.com>
2017-06-27 17:24:46 -0400

    added form

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {fetchPosts} from '../actions';
+import { Link } from 'react-router-dom';
 
 class PostIndex extends Component {
 	componentDidMount(){
@@ -8,19 +9,24 @@ class PostIndex extends Component {
 	}
 	renderPosts(){
 		 return _.map(this.props.posts, post=>{
-		 		return( 
+		 		return(
 		 			<li className="list-group-item" key={post.id}>
 		 				{post.title}
 		 			</li>
 		 			);
 
 		 	});
-		 	
+
 	}
 
 	render(){
 		return (
 			<div>
+				<div className="text-xs-right">
+					<Link className="btn btn-primary" to="/posts/new">
+						Add a Post
+					</Link>
+				</div>
 				<h3>Posts</h3>
 				<ul className="list-group">
 					{this.renderPosts()}
--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import {BrowserRouter, Route } from 'react-router-dom';
+import {BrowserRouter, Route, Switch } from 'react-router-dom';
 import reducers from './reducers';
 import PostIndex from './components/posts_index';
+import PostsNew from './components/posts_new';
 import promise from 'redux-promise';
 
 //on applyMiddleware we pass our middleware like redux promise to handle the request
@@ -15,7 +16,10 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
     	<div>
-	    	<Route path="/" component={PostIndex} />
+        <Switch>
+          <Route path="/posts/new" component={PostsNew} />
+          <Route path="/" component={PostIndex} /> // we have to put slash route last because of the lazy search of routes
+        </Switch>
 	    </div>
     </BrowserRouter>
   </Provider>
